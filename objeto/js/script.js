c33fc425c2b33aa8688c6dc6111e8142d3a59bb3
fobbe-coder <felipebarbosadagama@gmail.com>
2026-03-31 16:27:00 -0300

    Add files via upload

---
+++ objeto/js/script.js
@@ -0,0 +1,12 @@
+var objt = {
+    nome = "Felipe",
+    idade = 17,
+    Profissão = "Programador",
+    estatrabalhando = false
+};
+
+objt.estatrabalhando = true;
+objt.nome = "Roberto";
+console.log(objt.nome);
+
+console.log(objt);
